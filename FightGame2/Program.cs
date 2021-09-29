221608c936365dedaaa020c9e9becc9498e982e6
PLAYMANWAGON <aironas.keturka@elev.ga.ntig.se>
2021-09-29 13:52:31 +0200

    Initial Commit

---
+++ FightGame2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace FightGame2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Start
+
+            Character player = new Character();
+
+            Console.WriteLine("What is your Warrior's name?:\n");
+            player.name = Console.ReadLine();
+
+            Console.Clear();
+
+            Console.WriteLine("Welcome to FightGame2, \nPress enter to continue.");
+            Console.ReadLine();
+
+            Console.Clear();
+
+            Console.WriteLine(" __   __  _______  __    _  __   __ ║\n|  |_|  ||       ||  |  | ||  | |  |║\n|       ||    ___||   |_| ||  | |  |║\n|       ||   |___ |       ||  |_|  |║\n|       ||    ___||  _    ||       |║\n| ||_|| ||   |___ | | |   ||       |║\n|_|   |_||_______||_|  |__||_______|║\n=====================================");
+            Console.ReadLine();
+            Console.WriteLine("1. Go fight,       2. Go home.");
+
+            #endregion
+        }
+    }
+}
